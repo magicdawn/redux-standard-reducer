fb4b947ef13cb52d0a0a4db4cc731e68eff6d455
magicdawn <magicdawn@qq.com>
2017-08-20 22:17:25 +0800

    add more tests

--- test/simple.js
+++ test/simple.js
@@ -3,9 +3,13 @@ import { combineReducers } from 'redux'
 import reduceReducers from 'reduce-reducers'
 
 describe('Simple', function() {
-  it('It works', function() {
-    const reducer = reduceReducers(
+  let reducer
+  let state
+
+  beforeEach(() => {
+    reducer = reduceReducers(
       standardReducer,
+      (state = { arr: [] }, action) => state,
       combineReducers({
         partial1(state = { value: 1, foo: 'bar-1' }, action) {
           return state
@@ -17,13 +21,14 @@ describe('Simple', function() {
       })
     )
 
-    let state = {}
-
     // init
+    state = {}
     state = reducer(state, { type: 'init' })
     state.partial1.value.should.equal(1)
     state.partial2.value.should.equal(2)
+  })
 
+  it('It works', function() {
     // using type = STANDARD_MERGE_STATE_
     state = reducer(state, {
       type: 'STANDARD_MERGE_STATE_MODIFY_PARTIAL1',
@@ -49,5 +54,46 @@ describe('Simple', function() {
     })
     state.partial2.value.should.equal('modifyed-2')
     state.partial2.foo.should.equal('bar-2') // unchanged
+  })
+
+  it('skip when not ok', function() {
+    console.log(1111111111111111)
+    // no action
+    reducer(state, { foo: 'bar' }).partial2.value.should.equal(2)
+
+    // no payload
+    reducer(state, {
+      type: 'STANDARD_MERGE_STATE',
+    }).partial2.value.should.equal(2)
+
+    // no payload
+    reducer(state, {
+      type: 'ABCD',
+      standard: true,
+    }).partial2.value.should.equal(2)
+
+    // x - standard
+    // x - type
+    reducer(state, {
+      type: 'ABCD',
+      standard: false,
+      payload: {
+        partial1: {
+          value: 10,
+        },
+      },
+    }).partial2.value.should.equal(2)
+  })
+
+  it('with array', function() {
+    state = reducer(state, {
+      type: 'FETCH_SOME_LIST',
+      standard: true,
+      payload: {
+        arr: [4, 5, 6],
+      },
+    })
+
+    state.arr.should.match([4, 5, 6])
   })
 })
